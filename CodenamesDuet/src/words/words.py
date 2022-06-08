266d93e84848a350ec57f91777f0e42e6253dae6
Joseph T. Iosue <jtiosue@gmail.com>
2022-06-08 13:15:26 -0400

    Update words.py

--- CodenamesDuet/src/words/words.py
+++ CodenamesDuet/src/words/words.py
@@ -2,6 +2,10 @@ from random import sample, shuffle
 
 def random_words():
     """ Choose 25 random words from words.txt """
-    with open("words/words.txt") as f: l = sample(f.read().upper().split(), 25)
+    with open("words/words.txt") as f: 
+        l = [
+            x.strip()
+            for x in sample(f.read().upper().split("\n"), 25)
+        ]
     shuffle(l)
     return l
